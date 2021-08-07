754bea6baae49329655d47eee0a3144b8cc891e8
Akmal-M <akmal3017445@gmail.com>
2021-08-07 23:47:31 +0500

    change

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,76 @@
+const Router = require('express')
+const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const config = require('config')
+const jwt = require('jsonwebtoken')
+const {check, validationResult} = require('express-validator')
+
+const router = new Router()
+
+router.post('/registration',
+    [
+        check('email', 'Incorrect email').isEmail(),
+        check('password', 'Password must be longer than 6 and shorter than 12').isLength({min: 6, max: 12})
+    ],
+    async (req, res) => {
+        try {
+            console.log(req.body)
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({message: 'Incorrect request', errors})
+            }
+            const {email, password} = req.body;
+
+            const candidate = await User.findOne({email})
+
+            if (candidate) {
+                return res.status(400).json({message: `User with email ${email} already exist`})
+            }
+
+            const hashPassword = await bcrypt.hash(password, 15)
+            const user = new User({email, password: hashPassword})
+            await user.save()
+            return res.json({message: 'User was created'})
+
+        } catch (e) {
+            console.log(e)
+            res.send({message: 'Server error'})
+        }
+    }
+)
+
+
+router.post('/login',
+
+    async (req, res) => {
+        try {
+            const {email, password} = req.body;
+            const user = await User.findOne({email})
+            if (!user) {
+                return res.status(404).json({message: "User not found"})
+            }
+
+            const isPassValid = bcrypt.compareSync(password, user.password)
+
+            if(!isPassValid) {
+                return res.status(400).json({message: "Invalid password"})
+            }
+
+            const token = jwt.sign({id: user.id }, config.get('secretKey'), {expiresIn: '1h'})
+            return res.json({
+                token,
+                id: user.id,
+                email: user.email,
+                diskSpace: user.diskSpace,
+                usedSpace: user.usedSpace,
+                avatar: user.avatar,
+            })
+        } catch (e) {
+            console.log(e)
+            res.send({message: 'Server error'})
+        }
+    }
+)
+
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('config')
+const authRouter = require('./routes/auth.routes')
 
 const app = express();
 
 const PORT = config.get('serverPort')
 
+app.use(express.json())
+
+app.use('api/auth', authRouter)
+
 const start = async () => {
     try {
 
-        await mongoose.connect(config.get('dbUrl'))
+        await mongoose.connect(config.get('dbUrl'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
 
         app.listen(PORT, () => {
             console.log('server is running on port: ' + PORT)
